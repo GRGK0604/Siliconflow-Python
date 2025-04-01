9a73ed86969734e9d38110be3a9c7709d8bbddaf
归人过客 <86451386+GRGK0604@users.noreply.github.com>
2025-04-01 17:27:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 # if you want to add api key check, set this to the api key:
-API_KEY=None
+import os
+
+# 从环境变量读取API_KEY，如果没有则设为None
+API_KEY = os.environ.get("API_KEY", None)
 
 # Admin credentials for login
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "password"  # You should change this to a secure password
+# 从环境变量读取管理员账号密码，如果没有则使用默认值
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "password")  # You should change this to a secure password
